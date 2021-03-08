e843050ad98fa68fee7fcd376c04a6ac59199603
Eric Chen <airkro@qq.com>
2021-03-08 14:43:54 +0800

    chore: update

---
+++ test/transfer.cjs
@@ -0,0 +1,26 @@
+const test = require('ava');
+
+function findText(object, pattern) {
+  return JSON.stringify(object).matchAll(pattern).length;
+}
+
+test('qq', (t) => {
+  t.is(
+    findText(require('../src/qq.json'), /"id:"/g),
+    findText(require('../dist/qq.json'), /"code:"/g),
+  );
+});
+
+test('amap', (t) => {
+  t.is(
+    findText(require('../src/amap.json'), /"adcode:"/g),
+    findText(require('../dist/amap.json'), /"code:"/g),
+  );
+});
+
+test('npm', (t) => {
+  t.is(
+    findText(require('../src/npm.json'), /"code:"/g),
+    findText(require('../dist/npm.json'), /"code:"/g),
+  );
+});
---
+++ tool/index.cjs
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { Json } = require('fs-chain');
+
+const { AMAP, QQ } = require('@ladjs/env')();
+
+const qq = require('./qq.cjs');
+const amap = require('./amap.cjs');
+const npm = require('./npm.cjs');
+
+function action({ name, downloader, transfer }, key) {
+  if (key !== undefined) {
+    new Json()
+      .config({ pretty: true })
+      .handle(() => downloader(key))
+      .output(`../src/${name}.json`)
+      .handle(transfer)
+      .output(`../dist/${name}.json`)
+      .logger(name);
+  }
+}
+
+action(npm, false);
+action(qq, QQ);
+action(amap, AMAP);
